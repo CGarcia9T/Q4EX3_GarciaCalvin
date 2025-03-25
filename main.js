8805a8a44a7e76610132c3bcd6844bf6025be076
CGarcia9T <garcia.cu@obmontessori.edu.ph>
2025-03-25 11:31:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function determineGrade() {
+	let grade = document.getElementById('inputGrade').value;
+
+	if (grade <= 65) {
+		window.alert("You failed.")
+	}
+	else if (grade <= 75) {
+		window.alert("You are below average.")
+	}
+	else if (grade <= 80) {
+		window.alert("You're grade is average.")
+	}
+	else if (grade <= 85) {
+		window.alert("You are above average!")
+	}
+}
